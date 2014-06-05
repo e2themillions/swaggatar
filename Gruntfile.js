8babe2791ef2760b7097fdec98c54dafc6492eed
Emil Rossing <emil@lemur.dk>
2014-06-05 12:35:05 +0200

    added grunt-karma :boar:

--- Gruntfile.js
+++ Gruntfile.js
@@ -10,12 +10,22 @@ module.exports = function(grunt) {
         src: 'src/<%= pkg.name %>.js',
         dest: 'build/<%= pkg.name %>.min.js'
       }
+    },
+    karma: {
+      unit: {
+        configFile: 'karma.conf.js'
+      }
     }
   });
 
   // Load the plugin that provides the "uglify" task.
   grunt.loadNpmTasks('grunt-contrib-uglify');
 
+  // let's get som grunt karma
+  grunt.loadNpmTasks('grunt-karma');
+
   // Default task(s).
   grunt.registerTask('default', ['uglify']);
+
+
 };
